75bd44d12d0a724da6c954a7b742f7cf19533ab2
Guido Bouman <m@guido.vc>
2016-03-16 17:40:34 +0100

    Change version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'kadira:flow-router',
   summary: 'Carefully Designed Client Side Router for Meteor',
-  version: '2.10.1',
+  version: '3.0.0-custom',
   git: 'https://github.com/kadirahq/flow-router.git'
 });
 
